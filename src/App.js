6e5aaded2bae7cdbb1c31cb6d7fb029faee2bfa6
shivam bhardwaj <shivambhardwaj722@gmail.com>
2022-10-30 17:33:53 +0530

    upto enter items

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import Todolists from './Todolists';
+
+
 
 function App() {
+
+const[inputList , setInputList] = useState("");
+const[Items , setItems] = useState([]);
+
+const itemEvent = (event) => {
+
+  setInputList(event.target.value)
+
+};
+
+const listOfItems = () => {
+
+  setItems ( (oldItems) => {
+    return [...oldItems , inputList];
+  } )
+  setInputList("");
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main_div">
+
+      <div className='center_div'> 
+      <br/>
+
+          <h1> ToDo List </h1>
+          <br/>
+
+          <input type="text" placeholder='Add an item' value={inputList} onChange={itemEvent} />
+          <button onClick={listOfItems}> + </button>
+          <ol>
+             
+             {Items.map( (itemval) => {
+              
+               return <Todolists text={itemval}/>
+             })}
+
+          </ol>
+
+       </div>
+    
+          
+       
     </div>
   );
 }
---
+++ src/Todolists.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+ const Todolists = (props) => {
+  return (
+    <li> {props.text}</li>
+  )
+}
+
+export default Todolists;
